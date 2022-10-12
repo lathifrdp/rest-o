3c954a429a4698b37c212efe42a2bb2fc73d1b55
lathifrdp <lathifrdp@gmail.com>
2022-10-12 12:58:30 +0700

    theme colors

---
+++ lib/src/helper/theme_colors.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+
+Color primaryColorMain = const Color(0xff322B47);
+Color primaryColorSurface = const Color(0xffD6D5DA);
+Color primaryColorBorder = const Color(0xffBBB8C2);
+Color primaryColorDisable = const Color(0xff9895A3);
+
+Color baseColorMain = const Color(0xff127BD4);
+Color baseColorSurface = const Color(0xffD0E5F6);
+Color baseColorBorder = const Color(0xffB0D3F1);
+Color baseColorDisable = const Color(0xff88BDE9);
+
+Color successColorMain = const Color(0xff43936C);
+Color successColorSurface = const Color(0xffD9E9E2);
+Color successColorBorder = const Color(0xffC0DBCE);
+Color successColorDisable = const Color(0xff9BBCAC);
+
+Color warningColorMain = const Color(0xffB08800);
+Color warningColorSurface = const Color(0xffEFE7CC);
+Color warningColorBorder = const Color(0xffE5D7AA);
+Color warningColorDisable = const Color(0xffD7C380);
+
+Color errorColorMain = const Color(0xffBB0202);
+Color errorColorSurface = const Color(0xffF1CCCC);
+Color errorColorBorder = const Color(0xffE8ABAB);
+Color errorColorDisable = const Color(0xffDD8080);
+
+Color pinkColorMain = const Color(0xFFD412C1);
+Color pinkColorSurface = const Color(0xFFF5C6F0);
+
+Color darkPurpleColorMain = const Color(0xFF4F558C);
+Color darkPurpleColorSurface = const Color(0xFFD5D7E4);
+
+Color lightBlueColorMain = const Color(0xFF17E0D4);
+Color lightBlueColorSurface = const Color(0xFFC8F8F5);
+
+Color lightPurpleColorMain = const Color(0xFF9C71FF);
+Color lightPurpleColorSurface = const Color(0xFFE6DBFF);
+
+Color bluePurpleColorMain = const Color(0xFF5B6BFB);
+Color bluePurpleColorSurface = const Color(0xFFD8DCFE);
+
+Color lightGreenColorMain = const Color(0xFF5ADB5F);
+Color lightGreenColorSurface = const Color(0xFFCCFFDA);
+
+Color greyColorMain = const Color(0xFF807C8D);
+Color greyColorSurface = const Color(0xFFF7F7F8);
+
+Color neutralColor10 = const Color(0xffFFFFFF);
+Color neutralColor20 = const Color(0xffF7F7F8);
+Color neutralColor30 = const Color(0xffF1F0F2);
+Color neutralColor40 = const Color(0xffE6E6E9);
+Color neutralColor50 = const Color(0xffCECCD3);
+Color neutralColor60 = const Color(0xffB1AEB9);
+Color neutralColor70 = const Color(0xff908D9C);
+Color neutralColor80 = const Color(0xff807C8D);
+Color neutralColor90 = const Color(0xff656075);
+Color neutralColor100 = const Color(0xff3A334E);
+
+//IPBM Student Color
+Color blackColor = const Color(0xff000000);
+Color whiteColor = const Color(0xffFFFFFF);
+
+Color grayColor10 = const Color(0xffF9FAFB);
+Color grayColor20 = const Color(0xffF3F4F6);
+Color grayColor30 = const Color(0xffE5E7EB);
+Color grayColor40 = const Color(0xffD1D5DB);
+Color grayColor50 = const Color(0xff9CA3AF);
+Color grayColor60 = const Color(0xff6B7280);
+Color grayColor70 = const Color(0xff4B5563);
+Color grayColor80 = const Color(0xff374151);
+Color grayColor90 = const Color(0xff1F2937);
+Color grayColor100 = const Color(0xff111827);
+
+Color primaryColor10 = const Color(0xffCDE3FA);
+Color primaryColor20 = const Color(0xffACD1F6);
+Color primaryColor30 = const Color(0xff82B9F1);
+Color primaryColor40 = const Color(0xff58A2ED);
+Color primaryColor50 = const Color(0xff2F8BE8);
+Color primaryColor60 = const Color(0xff0574E4);
+Color primaryColor70 = const Color(0xff0461BE);
+Color primaryColor80 = const Color(0xff034D98);
+Color primaryColor90 = const Color(0xff023A72);
+Color primaryColor100 = const Color(0xff02274C);
+
+Color warningColor10 = const Color(0xffFFFBEB);
+Color warningColor20 = const Color(0xffFEF3C7);
+Color warningColor30 = const Color(0xffFDE68A);
+Color warningColor40 = const Color(0xffFCD34D);
+Color warningColor50 = const Color(0xffFBBF24);
+Color warningColor60 = const Color(0xffF59E0B);
+Color warningColor70 = const Color(0xffD97706);
+Color warningColor80 = const Color(0xffB45309);
+Color warningColor90 = const Color(0xff92400E);
+Color warningColor100 = const Color(0xff78350F);
+
+Color dangerColor10 = const Color(0xffFEF2F2);
+Color dangerColor20 = const Color(0xffFEE2E2);
+Color dangerColor30 = const Color(0xffFECACA);
+Color dangerColor40 = const Color(0xffFCA5A5);
+Color dangerColor50 = const Color(0xffF87171);
+Color dangerColor60 = const Color(0xffEF4444);
+Color dangerColor70 = const Color(0xffDC2626);
+Color dangerColor80 = const Color(0xffB91C1C);
+Color dangerColor90 = const Color(0xff991B1B);
+Color dangerColor100 = const Color(0xff7F1D1D);
+
+Color successColor10 = const Color(0xffECFDF5);
+Color successColor20 = const Color(0xffD1FAE5);
+Color successColor30 = const Color(0xffA7F3D0);
+Color successColor40 = const Color(0xff6EE7B7);
+Color successColor50 = const Color(0xff34D399);
+Color successColor60 = const Color(0xff10B981);
+Color successColor70 = const Color(0xff059669);
+Color successColor80 = const Color(0xff047857);
+Color successColor90 = const Color(0xff065F46);
+Color successColor100 = const Color(0xff064E3B);
+
+Color primaryNilaiA = const Color(0xff04CFA2);
+Color backgroundNilaiA = const Color(0xffCDF5EC);
+Color primaryNilaiAB = const Color(0xff40D27A);
+Color backgroundNilaiAB = const Color(0xffD9F6E4);
+Color primaryNilaiB = const Color(0xff7ED251);
+Color backgroundNilaiB = const Color(0xffE5F6DC);
+Color primaryNilaiBC = const Color(0xffBFD62A);
+Color backgroundNilaiBC = const Color(0xffF2F7D4);
+Color primaryNilaiC = const Color(0xffFED702);
+Color backgroundNilaiC = const Color(0xffFFF7CC);
+Color primaryNilaiD = const Color(0xffB8623A);
+Color backgroundNilaiD = const Color(0xffF1E0D8);
+Color primaryNilaiE = const Color(0xffA64137);
+Color backgroundNilaiE = const Color(0xffEDD9D7);
+Color primaryNilaiBL = const Color(0xffD1D5DB);
+Color backgroundNilaiBL = const Color(0xffF9FAFB);
